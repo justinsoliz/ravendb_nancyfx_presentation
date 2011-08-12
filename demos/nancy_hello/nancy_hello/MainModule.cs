7e8d7941cdfd10255808393ba44789e0635e6abc
Justin Soliz <justinsoliz@gmail.com>
2011-08-11 19:53:27 -0500

    Post presentation

---
+++ demos/nancy_hello/nancy_hello/MainModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace nancy_hello
+{
+    public class MainModule : NancyModule
+    {
+        public MainModule()
+        {
+            Get["/"] = x => "nancy says hello!";
+
+            Get["/razor-view"] = x => {
+                return View["test", new { Name = "Justin" }];
+            };
+        }
+    }
+
+    public class PublicModule : NancyModule
+    {
+        public PublicModule()
+            : base("/public")
+        {
+            Get["/css/{file}"] = x => {
+                return Response.AsCss("/public/css/" + (string)x.file);
+            };
+        }
+    }
+}
---
+++ demos/raven_hello/raven_hello/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Transactions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Raven.Client;
+using Raven.Client.Document;
+
+namespace raven_hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            using (IDocumentStore documentStore = new DocumentStore() {
+                Url = "http://localhost:8080"
+            }.Initialize())
+            {
+
+                using (IDocumentSession session = documentStore.OpenSession())
+                {
+                    var employees = session.Advanced.LuceneQuery<Employee>()
+                        .Where("Name:Justin")
+                        .FirstOrDefault();
+                }
+            }
+
+
+        }
+    }
+
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Friend { get; set; }
+    }
+}
